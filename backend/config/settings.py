c33fddcf7929943c1fcf18f0b057b46d768e797e
Rimmel Asghar <rimmelasghar3@gmail.com>
2023-09-29 19:39:40 +0500

    added dockerize postgresql

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -95,9 +95,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv("DB_NAME", 'newdb'),
+    'USER': os.getenv("DB_USER", 'postgres'),
+    'PASSWORD': os.getenv("DB_PASSWORD", '03322593149'),
+    'HOST': os.getenv("DB_HOST", 'localhost'),
+    'PORT': os.getenv("DB_PORT", 5432)
     }
 }
 
